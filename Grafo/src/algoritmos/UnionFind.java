c887b2255c5ab917eb6d922516da7591dd83f53a
juanma2208 <amnauju5@gmail.com>
2021-10-16 00:15:15 -0300

    unionFind testeado + unionFind test

--- Grafo/src/algoritmos/UnionFind.java
+++ Grafo/src/algoritmos/UnionFind.java
@@ -2,31 +2,37 @@ package algoritmos;
 
 public class UnionFind {
 	
-	private static int [] A ;
-	
-	public UnionFind (int vertices) {
-		int i= 0;
-		while (i < vertices) {
-			A[i]= i;
-			i++;
-		}
-		
-	}
-	static int root (int i) {
-		while (A[i]!= i) {
-			i = A[i];
-		}
-		return i;
-	}
-	
-	boolean find (int i, int j) {
-		return root(i) == root (j);
-	}
-	
-	static void Union(int i, int j)
-	{
-		int ri = root(i);
-		int rj = root (j);
-		A[ri] = rj;
-	}
+	private int[] parent;
+    public UnionFind(int n) {
+        parent = new int[n];
+        for (var i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+  //Busca la raiz del elemento dado por parametro
+    public int root(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+        // path compression
+        return parent[x] = root(parent[x]);
+    }
+ 
+  //Hace que la raiz de una de las componentes conexas
+  //pasadas por parametro apunte a la raiz de la otra
+    public void Union(int x, int y)  {
+        int px = root(x);
+        int py = root(y);
+        if (px != py) {
+            parent[px] = py;
+        }
+    }
+   //Determina si los elementos 
+   //pasados por parametros se encuentran en la misma componente conexa
+    public boolean find(int i,int j) {
+    	
+    	return root(i)==root(j);
+    	
+    }
+    
 }
---
+++ Grafo/src/algoritmos/UnionFindTest.java
@@ -0,0 +1,45 @@
+package algoritmos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UnionFindTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	
+	@Test
+	public void testUnion() {
+		UnionFind unionFind = new UnionFind(10);
+		assertEquals(unionFind.root(1), 1);
+	}
+	
+	@Test
+	public void testSuccessfulFind() {
+		   UnionFind unionFind = new UnionFind(12);
+		   unionFind.Union(1,2);
+
+		   assertTrue(unionFind.find(1,2));
+		}
+	@Test
+	public void testFailureFind() {
+	   UnionFind unionFind = new UnionFind(16);
+	   unionFind.Union(1,2);
+
+	   assertFalse(unionFind.find(1,3));
+	   }
+	@Test
+	public void testRoot() {
+		UnionFind unionFind = new UnionFind(6);
+		unionFind.Union(2,1);
+		assertEquals(unionFind.root(2),1);
+	}
+
+		
+
+	
+}
