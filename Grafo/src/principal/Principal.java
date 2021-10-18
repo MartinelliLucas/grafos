dbc5d0e288ea92a857ab5517a8bda450468f826a
NahuelMonin <nahuel.monin@gmail.com>
2021-10-18 11:42:33 -0300

    Terminada la clase principal donde correr el main y hacer las pruebas con ambas versiones de kruskal

--- Grafo/src/principal/Principal.java
+++ Grafo/src/principal/Principal.java
@@ -1,17 +1,29 @@
 package principal;
 
+import algoritmos.Kruskal;
+import bfs.BFS;
 import grafos.Grafo;
 
 public class Principal {
 
 	public static void main (String [] args) {
 		
-		int vertices = 0;
-		int aristas = 0;
+		Grafo grafoAleatorio = new Grafo(5, 2);
+		System.out.println("Grafo Conexo: " + BFS.esConexo(grafoAleatorio));
 		
-		Grafo grafoAleatorio = new Grafo(vertices, aristas);
+		Long antesKruskalBFS = System.currentTimeMillis();
+		Grafo grafoKruskalBFS = Kruskal.kruskalBFS(grafoAleatorio);
+		Long despuesKruskalBFS = System.currentTimeMillis();
+		Long tiempoEjecucionKruskalBFS = (despuesKruskalBFS - antesKruskalBFS);
 		
+		System.out.println("El tiempo de ejecucion de Kruskal con BFS fue de: " + tiempoEjecucionKruskalBFS + " milisegundos.");
 		
+		Long antesKruskalUnionFind = System.currentTimeMillis();
+		Grafo grafoKruskalUnionFind = Kruskal.kruskalUnionFind(grafoAleatorio);
+		Long despuesKruskalUnionFind = System.currentTimeMillis();
+		Long tiempoEjecucionKruskalUnionFind = despuesKruskalUnionFind - antesKruskalUnionFind;
 		
+		System.out.println("El tiempo de ejecucion de Kruskal con Union Find fue de: " + tiempoEjecucionKruskalUnionFind + " milisegundos.");
+	
 	}
 }
